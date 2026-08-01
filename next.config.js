b13d0b6bd499cf2799b2f54462a08e2a0b221736
kritirai-hyd <kritirai.hyd.com>
2026-08-02 00:14:40 +0530

    Fix CSS paths for custom domain

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
-const isProd = process.env.NODE_ENV === "production";
-
 module.exports = {
   output: "export",
   images: {
     unoptimized: true,
   },
-  basePath: isProd ? "/kriti_portfolio" : "",
-  assetPrefix: isProd ? "/kriti_portfolio/" : "",
 };
